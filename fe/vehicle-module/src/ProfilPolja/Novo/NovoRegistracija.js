b775667d0080e446124d1d48925a822dcf050d2a
veljkocukic <veljkocukic@gmail.com>
2021-12-02 15:17:14 +0100

    n

--- fe/vehicle-module/src/ProfilPolja/Novo/NovoRegistracija.js
+++ fe/vehicle-module/src/ProfilPolja/Novo/NovoRegistracija.js
@@ -6,16 +6,15 @@ import { useParams } from "react-router-dom"
 import { ZaposleniLista } from "./ZaposleniLista"
 
 export const NovoRegistracija = ({ newC }) => {
-    let { valid, setValid, setNewOn, spinerOn, setSpinerOn, verDate,dateReg, setDateReg, docReg, setDocReg, troskovi, setTroskovi, registrovao, setRegistrovao, timeZaposleni, setTimeZaposleni, regDo, setRegDo } = useContext(DataContext)
+    let { valid, setValid, setNewOn, spinerOn, setSpinerOn, verDate, dateReg, setDateReg, docReg, setDocReg, troskovi, setTroskovi, registrovao, setRegistrovao, timeZaposleni, setTimeZaposleni, regDo, setRegDo } = useContext(DataContext)
 
 
     let { carId } = useParams()
-    let [dateFalse,setDateFalse] = useState(false)
-    let [docFalse,setDocFalse] = useState(false)
-    let [trosFalse,setTrosFalse] = useState(false)
-    let [regFalse,setRegFalse] = useState(false)
-    let [timeFalse,setTimeFasle] = useState(false)
-    let [doFalse,setDoFalse]  = useState(false)
+    let [dateFalse, setDateFalse] = useState(false)
+    let [docFalse, setDocFalse] = useState(false)
+    let [trosFalse, setTrosFalse] = useState(false)
+    let [timeFalse, setTimeFasle] = useState(false)
+    let [doFalse, setDoFalse] = useState(false)
 
 
     const handleSubmit = async () => {
@@ -24,7 +23,7 @@ export const NovoRegistracija = ({ newC }) => {
         let verifyDoc = docReg.length > 3
         let verifyTroskovi = troskovi > 0
         let verifyRegistrovao = registrovao.length > 2
-        let verifyTime = timeZaposleni>0
+        let verifyTime = timeZaposleni > 0
         let verifyDo = verDate(regDo)
         if (verifyDate && verifyDoc && verifyTroskovi && verifyRegistrovao && verifyTime && verifyDo) {
             await axios.post("http://localhost:5000/api/v1/registracija/new/" + carId, { dateReg, docReg, troskovi, registrovao, timeZaposleni, regDo }).then(res => {
@@ -47,7 +46,6 @@ export const NovoRegistracija = ({ newC }) => {
             !verifyDate ? setDateFalse(true) : setDateFalse(false)
             !verifyDoc ? setDocFalse(true) : setDocFalse(false)
             !verifyTroskovi ? setTrosFalse(true) : setTrosFalse(false)
-            !verifyRegistrovao ? setRegFalse(true) : setTrosFalse(false)
             !verifyTime ? setTimeFasle(true) : setTimeFasle(false)
             !verifyDo ? setDoFalse(true) : setDoFalse(false)
 
@@ -55,7 +53,7 @@ export const NovoRegistracija = ({ newC }) => {
         }
     }
 
-    const handleCancel=()=>{
+    const handleCancel = () => {
 
         setValid(true)
         setNewOn(false)
@@ -69,15 +67,15 @@ export const NovoRegistracija = ({ newC }) => {
 
                 <div className="single-input-container">
                     <label htmlFor="datum-registracije" className="standard--label">Datum registracije <span>*</span></label>
-                    <input style={{border:dateFalse&&"1px solid red"}} onBlur={e=>!verDate(e.target.value) ? setDateFalse(true) : setDateFalse(false) } onChange={e => setDateReg(e.target.value)} type="date" className="standard--input" id="datum-registracije" name="datum-registracije" />
-                    { dateFalse && <p style={{color:"red",fontSize:".8em"}}>Datum mora biti validan</p>}
+                    <input style={{ border: dateFalse && "1px solid red" }} onBlur={e => !verDate(e.target.value) ? setDateFalse(true) : setDateFalse(false)} onChange={e => setDateReg(e.target.value)} type="date" className="standard--input" id="datum-registracije" name="datum-registracije" />
+                    {dateFalse && <p style={{ color: "red", fontSize: ".8em" }}>Datum mora biti validan</p>}
                 </div>
 
 
                 <div className="single-input-container">
                     <label htmlFor="troskovi-registracije" className="standard--label">Troškovi registracije <span>*</span></label>
-                    <input style={{border:trosFalse&&"1px solid red"}} onBlur={e=>e.target.value===""||e.target.value<=0 ? setTrosFalse(true) : setTrosFalse(false)} type="number" onChange={(e) => setTroskovi(e.target.value)} className="standard--input" id="troskovi-registracije" name="troskovi-registracije" />
-                    { trosFalse && <p style={{color:"red",fontSize:".8em"}}>Broj mora biti veći od 0</p>}
+                    <input style={{ border: trosFalse && "1px solid red" }} onBlur={e => e.target.value === "" || e.target.value <= 0 ? setTrosFalse(true) : setTrosFalse(false)} type="number" onChange={(e) => setTroskovi(e.target.value)} className="standard--input" id="troskovi-registracije" name="troskovi-registracije" />
+                    {trosFalse && <p style={{ color: "red", fontSize: ".8em" }}>Broj mora biti veći od 0</p>}
                 </div>
 
                 <div className="single-input-container">
@@ -87,20 +85,20 @@ export const NovoRegistracija = ({ newC }) => {
 
                 <div className="single-input-container">
                     <label htmlFor="vreme-zaposlenog" className="standard--label">Vreme zaposlenog <span>*</span></label>
-                    <input style={{border:timeFalse&&"1px solid red"}} onBlur={e=>e.target.value===""||e.target.value<=0 ? setTimeFasle(true) : setTimeFasle(false)} onChange={(e) => setTimeZaposleni(e.target.value)} type="text" className="standard--input" id="vreme-zaposlenog" name="vreme-zaposlenog" />
-                    { timeFalse && <p style={{color:"red",fontSize:".8em"}}>Broj mora biti veći od 0</p>}
+                    <input style={{ border: timeFalse && "1px solid red" }} onBlur={e => e.target.value === "" || e.target.value <= 0 ? setTimeFasle(true) : setTimeFasle(false)} onChange={(e) => setTimeZaposleni(e.target.value)} type="text" className="standard--input" id="vreme-zaposlenog" name="vreme-zaposlenog" />
+                    {timeFalse && <p style={{ color: "red", fontSize: ".8em" }}>Broj mora biti veći od 0</p>}
                 </div>
 
                 <div className="single-input-container">
                     <label htmlFor="registrovan-do" className="standard--label">Registrovan do <span>*</span></label>
-                    <input style={{border:doFalse&&"1px solid red"}} onBlur={e=>!verDate(e.target.value) ? setDoFalse(true) : setDoFalse(false)} onChange={(e) => setRegDo(e.target.value)} type="date" className="standard--input" id="registrovan-do" name="registrovan-do" />
-                    { doFalse && <p style={{color:"red",fontSize:".8em"}}>Datum mora biti validan</p>}
+                    <input style={{ border: doFalse && "1px solid red" }} onBlur={e => !verDate(e.target.value) ? setDoFalse(true) : setDoFalse(false)} onChange={(e) => setRegDo(e.target.value)} type="date" className="standard--input" id="registrovan-do" name="registrovan-do" />
+                    {doFalse && <p style={{ color: "red", fontSize: ".8em" }}>Datum mora biti validan</p>}
                 </div>
 
                 <div className="single-input-container">
                     <label htmlFor="dokumentacija" className="standard--label">Dokumentacija <span>*</span></label>
-                    <textarea style={{border:docFalse&&"1px solid red"}} onBlur={e=>e.target.value<4 ? setDocFalse(true) : setDocFalse(false)} onChange={(e) => setDocReg(e.target.value)} className="standard--input" id="dokumentacije" name="dokumentacija" ></textarea>
-                    { docFalse && <p style={{color:"red",fontSize:".8em"}}>Unos mora biti veći od 3 karaktera</p>}
+                    <textarea style={{ border: docFalse && "1px solid red" }} onBlur={e => e.target.value < 4 ? setDocFalse(true) : setDocFalse(false)} onChange={(e) => setDocReg(e.target.value)} className="standard--input" id="dokumentacije" name="dokumentacija" ></textarea>
+                    {docFalse && <p style={{ color: "red", fontSize: ".8em" }}>Unos mora biti veći od 3 karaktera</p>}
                 </div>
 
             </div>
--- fe/vehicle-module/src/ProfilPolja/Registacija.js
+++ fe/vehicle-module/src/ProfilPolja/Registacija.js
@@ -5,19 +5,21 @@ import { Dialog } from "./Editi/Dialog.js"
 import { useParams } from "react-router-dom"
 import { NovoRegistracija } from "./Novo/NovoRegistracija"
 import { Paginacija } from "../Paginacija"
+import { ZaposleniLista } from "./Novo/ZaposleniLista"
 
 export const Registracija = ({ registracijaAr }) => {
-    let { verDate,minsToTime, newOn, setNewOn, setId, formatDate, setOpenRegEdit, openRegEdit, openDialog, setOpenDialog } = useContext(DataContext)
-    let [sliceAr,setSliceAr] = useState(registracijaAr.slice(0,5))
+    let { verDate, minsToTime, newOn, setNewOn, setId, formatDate, setOpenRegEdit, openRegEdit, openDialog, setOpenDialog } = useContext(DataContext)
+    let [sliceAr, setSliceAr] = useState(registracijaAr.slice(0, 5))
     let [regId, setRegId] = useState("")
+    let [filterOn, setFilterOn] = useState("")
     let { carId } = useParams()
 
 
-    useEffect(()=>{
-        setSliceAr(registracijaAr.slice(0,5))
+    useEffect(() => {
+        setSliceAr(registracijaAr.slice(0, 5))
         console.log(2)
 
-    },[registracijaAr])
+    }, [registracijaAr])
 
     const handleRegEditOpen = (_id) => {
         setRegId(_id)
@@ -32,47 +34,117 @@ export const Registracija = ({ registracijaAr }) => {
     }
 
     const Kolone = (props) => {
-        
+
         return (
             <tr>
-                <td>{!verDate(props.date) ? "/" :formatDate(props.date)}</td>
+                <td>{!verDate(props.date) ? "/" : formatDate(props.date)}</td>
                 <td>{props.doc}</td>
                 <td>{props.reg}</td>
                 <td>{props.user}</td>
-                <td>{props.time==="/" ? "/" : minsToTime(props.time)}</td>
+                <td>{props.time === "/" ? "/" : minsToTime(props.time)}</td>
                 <td>{!verDate(props.expire) ? "/" : formatDate(props.expire)}</td>
                 <td><button className="btn" onClick={() => handleRegEditOpen(props.kid)}><i className="fas fa-edit"></i> IZMENI</button><button className="btn del" onClick={() => handleDelete(props.kid)}> <i className="far fa-trash-alt"></i> OBRIŠI</button></td>
             </tr>
         )
     }
-    let cond = registracijaAr.length===1
+    let cond = registracijaAr.length === 1
 
     return (
         <div>
             {openRegEdit && <EditRegistracija registracijaAr={registracijaAr} regId={regId} carId={carId} />}
-        <table className="tg">
+            <table className="tg">
 
-            <thead>
-                <tr>
-                    <th colSpan="7" >Registracija</th>
-                </tr>
-                <tr className="head-table" >
-                    <th>Datum registracije</th>
-                    <th>Dokumentacija</th>
-                    <th>Troškovi registracije</th>
-                    <th>Registrovao zaposleni</th>
-                    <th>Vreme zaposlenog</th>
-                    <th>Registrovan do</th>
-                    <th className="tg-0pky"><button className="editBtn" onClick={() => setNewOn(true)}><i className="fas fa-plus"></i>Novo</button></th>
-                </tr>
-            </thead>
-            <tbody>
-                {sliceAr.map((item, key) => <Kolone kid={cond ? "/" : item._id} date={cond ? "/" : item.datum} doc={cond ? "/" : item.dokumentacija} reg={cond ? "/" : item.cena.toLocaleString()} user={cond ? "/" : item.registrovaoZaposleni} time={cond ? "/" : item.vremeZaposlenog} expire={item.registrovanDo} key={key} />)}
-            </tbody>
-        </table>
-                {newOn && <NovoRegistracija />}
-                {openDialog && <Dialog par={carId} polje="reg" />}
-                <Paginacija sliceAr={sliceAr} setSliceAr={setSliceAr} arr={registracijaAr}/>
+                <thead>
+                    <tr>
+                        <div className="filter-toggle" onClick={() => setFilterOn(prev => !prev)}><i class="fas fa-filter"></i> Filter
+                        </div>
+                        <th colSpan="7" >Registracija</th>
+                    </tr>
+                    <th> {filterOn && <Filter />}</th>
+                    <tr className="head-table" >
+                        <th>Datum registracije</th>
+                        <th>Dokumentacija</th>
+                        <th>Troškovi registracije</th>
+                        <th>Registrovao zaposleni</th>
+                        <th>Vreme zaposlenog</th>
+                        <th>Registrovan do</th>
+                        <th className="tg-0pky"><button className="editBtn" onClick={() => setNewOn(true)}><i className="fas fa-plus"></i>Novo</button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sliceAr.map((item, key) => <Kolone kid={cond ? "/" : item._id} date={cond ? "/" : item.datum} doc={cond ? "/" : item.dokumentacija} reg={cond ? "/" : item.cena.toLocaleString()} user={cond ? "/" : item.registrovaoZaposleni} time={cond ? "/" : item.vremeZaposlenog} expire={item.registrovanDo} key={key} />)}
+                </tbody>
+            </table>
+            {newOn && <NovoRegistracija />}
+            {openDialog && <Dialog par={carId} polje="reg" />}
+            <Paginacija sliceAr={sliceAr} setSliceAr={setSliceAr} arr={registracijaAr} />
         </div>
+    )
+}
+
+const Filter = () => {
+    return (
+        <div className="input--container filter-container" >
+            <h3 className="input--container__title">Filter</h3>
+            <div className="form" >
+
+
+                <div className="single-input-container">
+                    <label htmlFor="datum-registracije-od-fil" className="standard--label">Datum registracije od<span>*</span></label>
+                    <input type="date" className="standard--input" id="datum-registracije-oo-fil" name="datum-registracije-od-fil" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="datum-registracije-do-fil" className="standard--label">Datum registracije do<span>*</span></label>
+                    <input type="date" className="standard--input" id="datum-registracije-do-fil" name="datum-registracije-do-fil" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="datum-registracije-do-fil" className="standard--label">Dokumentacija<span>*</span></label>
+                    <input type="text" className="standard--input" id="dokum-fil" name="dokum-fil" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="tros-fill-od" className="standard--label">Troškovi od<span>*</span></label>
+                    <input type="number" className="standard--input" id="tros-fill-od" name="tros-fill-od" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="tros-fill-do" className="standard--label">Troškovi do<span>*</span></label>
+                    <input type="number" className="standard--input" id="tros-fill-do" name="tros-fill-do" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="reg-fill" className="standard--label">Registrovao zaposleni<span>*</span></label>
+                    <ZaposleniLista />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="vreme-fill-od" className="standard--label">Vreme zaposlenog od<span>*</span></label>
+                    <input type="number" className="standard--input" id="vreme-fill-od" name="vreme-fill-od" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="vreme-fill-do" className="standard--label">Vreme zaposlenog do<span>*</span></label>
+                    <input type="number" className="standard--input" id="vreme-fill-do" name="vreme-fill-do" />
+                </div>
+
+                <div className="single-input-container">
+                    <label htmlFor="exp-fill" className="standard--label">Registrovan do<span>*</span></label>
+                    <input type="date" className="standard--input" id="exp-fill" name="exp-fill" />
+                </div>
+
+                <button className="btn yes" ><i className="far fa-save"></i> PRIMENI</button>
+
+
+
+
+
+
+
+            </div>
+
+        </div>
+
     )
 }
