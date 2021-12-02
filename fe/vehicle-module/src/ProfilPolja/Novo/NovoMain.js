edf95d409b604ca776c493b651c9f356cccce8b3
veljkocukic <veljkocukic@gmail.com>
2021-12-02 11:01:04 +0100

    Update NovoMain.js

--- fe/vehicle-module/src/ProfilPolja/Novo/NovoMain.js
+++ fe/vehicle-module/src/ProfilPolja/Novo/NovoMain.js
@@ -6,14 +6,14 @@ import { ZaposleniLista } from "./ZaposleniLista"
 export const NovoMain = ({ setEditOn }) => {
 
 
-    let { verReg, valid, setValid, setNewOn, spinerOn, setSpinerOn, verDate, marka, setMarka, regBr, setRegBr, typeMn, setTypeMn, korisnikMn, setKorisnikMn, aktivnoDo, setAktivnoDo, aktivnoOd, setAktivnoOd,regDo, setRegDo } = useContext(DataContext)
+    let { verReg, valid, setValid, setNewOn, spinerOn, setSpinerOn, verDate, marka, setMarka, regBr, setRegBr, typeMn, setTypeMn, korisnikMn, setKorisnikMn, aktivnoDo, setAktivnoDo, aktivnoOd, setAktivnoOd, regDo, setRegDo } = useContext(DataContext)
     let [zaposleni, setZaposleni] = useState(true)
     let [file, setFile] = useState([])
 
-    let [markaFalse,setMarkaFalse] = useState(false)
-    let [regFalse,setRegFalse] = useState(false)
-    let [fromFalse,setFromFalse] = useState(false)
-    let [doFalse,setDoFalse] = useState(false)
+    let [markaFalse, setMarkaFalse] = useState(false)
+    let [regFalse, setRegFalse] = useState(false)
+    let [fromFalse, setFromFalse] = useState(false)
+    let [doFalse, setDoFalse] = useState(false)
 
     let handleSubmit = async () => {
         setSpinerOn(true)
@@ -27,7 +27,7 @@ export const NovoMain = ({ setEditOn }) => {
 
         if (verifyMarka && verifyReg && verifyActiveFrom && verifyDo) {
             await axios.post("http://localhost:5000/api/v1/main", {
-                marka, regBr, korisnikMn, aktivnoOd, aktivnoDo, regDo, typeMn
+                marka, regBr, korisnikMn, aktivnoOd, aktivnoDo, regDo, typeMn, file
             })
                 .then(() => setSpinerOn(false))
                 .catch(er => console.log(er))
@@ -40,9 +40,9 @@ export const NovoMain = ({ setEditOn }) => {
             setValid(false)
             setSpinerOn(false)
             !verifyMarka ? setMarkaFalse(true) : setMarkaFalse(false)
-            !verifyReg ? setRegFalse(true): setRegFalse(false)
-            !verifyActiveFrom ? setFromFalse(true): setFromFalse(false)
-            !verifyDo ? setDoFalse(true): setDoFalse(false)
+            !verifyReg ? setRegFalse(true) : setRegFalse(false)
+            !verifyActiveFrom ? setFromFalse(true) : setFromFalse(false)
+            !verifyDo ? setDoFalse(true) : setDoFalse(false)
 
             console.log(verifyMarka, verifyReg, verifyActiveFrom, verifyDo)
         }
@@ -111,15 +111,15 @@ export const NovoMain = ({ setEditOn }) => {
             <form className="form" encType="multipart/form-data">
                 <div className="single-input-container">
                     <label htmlFor="marka-tip" className="standard--label">Marka i tip <span>*</span></label>
-                    <input style={{border: markaFalse&&"1px solid red"}} onBlur={e => e.target.value.length <= 4 ? setMarkaFalse(true) : setMarkaFalse(false)} onChange={e => setMarka(e.target.value)} type="text" className="standard--input" id="marka-tip" name="marka-tip" />
-                    { markaFalse && <p style={{color:"red",fontSize:".8em"}}>Unos mora biti duži od 4 karaktera</p>}
+                    <input style={{ border: markaFalse && "1px solid red" }} onBlur={e => e.target.value.length <= 4 ? setMarkaFalse(true) : setMarkaFalse(false)} onChange={e => setMarka(e.target.value)} type="text" className="standard--input" id="marka-tip" name="marka-tip" />
+                    {markaFalse && <p style={{ color: "red", fontSize: ".8em" }}>Unos mora biti duži od 4 karaktera</p>}
                 </div>
 
 
                 <div className="single-input-container">
                     <label htmlFor="registracioni-broj" className="standard--label">Registracioni broj <span>*</span></label>
-                    <input style={{border: regFalse&&"1px solid red"}} onBlur={e => handleBlurReg(e)} onChange={e => setRegBr(e.target.value)} type="text" className="standard--input" id="registracioni-broj" name="registracioni-broj" />
-                    { regFalse && <p style={{color:"red",fontSize:".8em"}}>Format registracije mora biti ispravan</p>}
+                    <input style={{ border: regFalse && "1px solid red" }} onBlur={e => handleBlurReg(e)} onChange={e => setRegBr(e.target.value)} type="text" className="standard--input" id="registracioni-broj" name="registracioni-broj" />
+                    {regFalse && <p style={{ color: "red", fontSize: ".8em" }}>Format registracije mora biti ispravan</p>}
                 </div>
 
                 <div className="single-input-container">
@@ -137,15 +137,15 @@ export const NovoMain = ({ setEditOn }) => {
 
                 <div className="single-input-container">
                     <label htmlFor="registrovan-do" className="standard--label">Registrovano do <span>*</span></label>
-                    <input style={{border: doFalse&&"1px solid red"}} onBlur={e => e.target.value === "" ? setDoFalse(true):setDoFalse(false)} onChange={e => setRegDo(e.target.value)} type="date" className="standard--input" id="registrovan-do" name="registrovan-do" />
-                    { doFalse && <p style={{color:"red",fontSize:".8em"}}>Datum mora biti validan</p>}
+                    <input style={{ border: doFalse && "1px solid red" }} onBlur={e => e.target.value === "" ? setDoFalse(true) : setDoFalse(false)} onChange={e => setRegDo(e.target.value)} type="date" className="standard--input" id="registrovan-do" name="registrovan-do" />
+                    {doFalse && <p style={{ color: "red", fontSize: ".8em" }}>Datum mora biti validan</p>}
 
                 </div>
 
                 <div className="single-input-container">
                     <label htmlFor="aktivno-od" className="standard--label">Vozilo aktivno od <span>*</span></label>
-                    <input style={{border: fromFalse&&"1px solid red"}} onBlur={e => e.target.value === "" ? setFromFalse(true):setFromFalse(false)} onChange={e => setAktivnoOd(e.target.value)} type="date" className="standard--input" id="aktivno-od" name="aktivno-od" />
-                    { fromFalse && <p style={{color:"red",fontSize:".8em"}}>Datum mora biti validan</p>}
+                    <input style={{ border: fromFalse && "1px solid red" }} onBlur={e => e.target.value === "" ? setFromFalse(true) : setFromFalse(false)} onChange={e => setAktivnoOd(e.target.value)} type="date" className="standard--input" id="aktivno-od" name="aktivno-od" />
+                    {fromFalse && <p style={{ color: "red", fontSize: ".8em" }}>Datum mora biti validan</p>}
                 </div>
 
                 <div className="single-input-container">
